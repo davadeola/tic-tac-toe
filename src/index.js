c2e97c6ff37dd3b51999854caefc98a86ca70847
David Adeola <adeoladavid15@gmail.com>
2018-04-13 12:14:38 +0300

    MAde the game to decide the winner

--- src/index.js
+++ src/index.js
@@ -22,8 +22,11 @@ class Board extends React.Component {
 
   handleClick(i){
     const squares = this.state.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
     squares[i] = this.state.xIsNext ? "X" : "O";
-    this.setState({squares: squares, xIsNext: false});
+    this.setState({squares: squares, xIsNext: !this.state.xIsNext});
   }
 
   renderSquare(i) {
@@ -36,7 +39,10 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: ' + (this.state.xIsNext ? 'x' : 'o');
+    const winner = calculateWinner(this.state.squares);
+    const status = winner ? 'Winner: '+ winner : 'Next player: ' + (this.state.xIsNext ? 'x' : 'o');
+
+
 
     return (
       <div>
@@ -83,3 +89,24 @@ ReactDOM.render(
   <Game />,
   document.getElementById('root')
 );
+//helper function
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
